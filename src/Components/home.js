050270cb6983a0873ff28b0c7aabfcec85bbdd96
ItsVaishnav <vaishnavghadgepc99@gmail.com>
2025-01-23 23:26:54 +0530

    Commit

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 export default function home(){
+  // let slideIndex = 0;
+  // showSlides();
+
+  // const showSlides = () => {
+  //     let i;
+  //     let slides = document.getElementsByClassName("mySlides");
+  //     for (i = 0; i < slides.length; i++) {
+  //         slides[i].style.display = "none";
+  //     }
+  //     slideIndex++;
+  //     if (slideIndex > slides.length) {
+  //         slideIndex = 1
+  //     }
+  //     slides[slideIndex - 1].style.display = "block";
+  //     setTimeout(showSlides, 3000); // Change image every 3 seconds
+  // }
+
+  // const [slide , setslide] = useState(None);
+
+
   return (
     <>
       <div className="main">
